2740f45bf3575cbf1fd10f84a4644817e1ec7058
WillMoss11 <william.moss@keyin.com>
2025-02-05 00:15:59 -0330

    Comment Update

--- src/main/java/BankingApp/TransactionService.java
+++ src/main/java/BankingApp/TransactionService.java
@@ -2,12 +2,12 @@ package BankingApp;
 
 public class TransactionService {
 
-    // Method to process a deposit
+    // The method to process a deposit
     public void processDeposit(BankAccount account, double amount) {
         account.deposit(amount);
     }
 
-    // Method to process a withdrawal
+    // The method to process a withdrawal
     public void processWithdrawal(BankAccount account, double amount) {
         account.withdraw(amount);
     }
