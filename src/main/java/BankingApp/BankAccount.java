34c5cdcb447090001ae63da64ac65fdf02a78149
WillMoss11 <william.moss@keyin.com>
2025-02-04 21:29:54 -0330

    Added Balance/Depo/Withdraw Methods

--- src/main/java/BankingApp/BankAccount.java
+++ src/main/java/BankingApp/BankAccount.java
@@ -9,6 +9,33 @@ public class BankAccount {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
     }
+
+    // Method to get the balance of the account
+    public double getBalance() {
+        return balance;
+    }
+
+
+    // Method to deposit money into the account
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            throw new IllegalArgumentException("Deposit amount must be greater than 0.");
+        }
+    }
+
+
+    // Method to withdraw money from the account
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else if (amount <= 0) {
+            throw new IllegalArgumentException("Withdraw amount must be greater than 0.");
+        } else {
+            throw new IllegalArgumentException("Insufficient funds.");
+        }
+    }
 }
 
 
