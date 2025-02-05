bee2cf35bf48dac5f6dc143250a7cf94bcc98d03
WillMoss11 <william.moss@keyin.com>
2025-02-04 21:37:00 -0330

    Added Deposit Method

--- src/main/java/BankingApp/TransactionService.java
+++ src/main/java/BankingApp/TransactionService.java
@@ -1,5 +1,13 @@
 package BankingApp;
 
 public class TransactionService {
+
+    // Method to process a deposit
+    public void processDeposit(BankAccount account, double amount) {
+        account.deposit(amount);
+    }
+
+    // Method to process a withdrawal
+
 }
 
