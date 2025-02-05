62ec183407482364e5576ca30539497dcc198560
WillMoss11 <william.moss@keyin.com>
2025-02-04 21:19:40 -0330

    Added File Starts

--- src/main/java/BankingApp/BankAccount.java
+++ src/main/java/BankingApp/BankAccount.java
@@ -1,4 +1,12 @@
 package BankingApp;
 
 public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    // Constructor to initialize account number and balance
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
 }
